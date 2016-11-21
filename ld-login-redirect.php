45bafb8d606ee349efd0ab808092c5d20dad6cfc
Bardia Soleimanzadeh <bardia@loldesign.no>
2016-11-21 18:35:58 +0100

    camelCase removal ++

--- ld-login-redirect.php
+++ ld-login-redirect.php
@@ -1,18 +1,18 @@
 <?php
-/*
-  Plugin Name: LD Login Redirect
-  Description: Redirects "/logginn" to wp-login.php
-  Version: 1.0
-  Author: LOL Design
-  Author URI: http://loldesign.no
+/**
+ * Plugin Name: LD Login Redirect
+ * Description: Redirects "/logginn" to wp-login.php
+ * Version: 1.0
+ * Author: LOL Design
+ * Author URI: http://loldesign.no
  */
  
 function ld_login_redirect() {
-	$urlBeeingAccessed = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-	if (strpos($urlBeeingAccessed,'logginn') !== false) {
-	$loginUrl = home_url() . "/wp-login.php"; 
-	wp_redirect( $loginUrl );
-	exit;
+	$url_accessed = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+	if ( strpos($url_accessed, 'logginn') !== false ) {
+		$loginUrl = home_url() . "/wp-login.php"; 
+		wp_redirect( $loginUrl );
+		exit;
 	}
 }
 
